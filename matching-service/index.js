2c80bb57804f121da415d00772d54a18e2bf36b4
joszx <joshua_rockz@hotmail.com>
2022-09-01 11:46:02 +0800

    add socket.io server instance for matching service

--- matching-service/index.js
+++ matching-service/index.js
@@ -1,17 +1,26 @@
-import express from 'express';
-import cors from 'cors';
-import { createServer } from 'http';
+import express from "express";
+import cors from "cors";
+import { createServer } from "http";
+import { Server } from "socket.io";
 
 const app = express();
-app.use(express.urlencoded({ extended: true }))
-app.use(express.json())
-app.use(cors()) // config cors so that front-end can use
-app.options('*', cors())
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cors()); // config cors so that front-end can use
+app.options("*", cors());
 
-app.get('/', (req, res) => {
-    res.send('Hello World from matching-service');
+app.get("/", (req, res) => {
+  res.send("Hello World from matching-service");
 });
 
-const httpServer = createServer(app)
+const httpServer = createServer(app);
+
+const io = new Server(httpServer, {
+  /* options */
+});
+
+io.on("connection", (socket) => {
+  console.log("listening on *:8001");
+});
 
 httpServer.listen(8001);
