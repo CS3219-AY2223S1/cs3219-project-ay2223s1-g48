fa087383362117e89223fd8c5fe78739b940f74b
joszx <joshua_rockz@hotmail.com>
2022-09-11 00:18:15 +0800

    add event listener in socket.io socket controller to create new match

--- matching-service/index.js
+++ matching-service/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import { createServer } from "http";
 import { Server } from "socket.io";
+import { ormCreateMatch } from "./model/matching-orm.js";
 
 const app = express();
 app.use(express.urlencoded({ extended: true }));
@@ -20,7 +21,11 @@ const io = new Server(httpServer, {
 });
 
 io.on("connection", (socket) => {
-  console.log("listening on *:8001");
+  console.log("listening on :8001");
+
+  socket.on("match", function (data) {
+    ormCreateMatch(data.username, data.difficulty);
+  });
 });
 
 httpServer.listen(8001);
