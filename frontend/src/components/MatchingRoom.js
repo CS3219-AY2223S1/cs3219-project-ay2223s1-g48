11292bdedb672d7024a2287bd479f979bf193410
lchokhoe <chokhoeefps335f@gmail.com>
2022-10-05 17:01:38 +0800

    Update MatchingRoom.js and index.css
    Created a placeholder for the desired layout of the matching room.

--- frontend/src/components/MatchingRoom.js
+++ frontend/src/components/MatchingRoom.js
@@ -9,15 +9,37 @@ const MatchingRoom = () => {
           });
     }
     const location = useLocation()
-    return ( 
-        <div>
-            <h1>
-                Welcome { location.state.username }, to matching room { location.state.matchedRoomId }!
-            </h1>
-            <h2>
-                <button className="returnButton"onClick={() => handleReturn()}>Return</button>
-            </h2>
+    return (
+        <div className="titleandservices">
+            <div className="titles">
+                <div className="title">
+                    Welcome { location.state.username }, to matching room { location.state.matchedRoomId }!
+                </div>
+                <div className="returnbutton">
+                    <button className="returnButton"onClick={() => handleReturn()}>Return</button>
+                </div>
+            </div>
+            <div className="services">
+                <div className="collabservice">
+                    <textarea name="collab" cols="30" rows="10" placeholder="Waiting for coding to start...">
+                        
+                    </textarea>
+                </div>
+                <div className="topbottom">
+                    <div className="questionservice">
+                        <textarea name="question" cols="30" rows="10" placeholder="Your question goes here...">
+
+                        </textarea>
+                    </div>
+                    <div className="communicationservice">
+                        <textarea name="communication" cols="30" rows="10" placeholder="Waiting for someone to speak...">
+
+                        </textarea>
+                    </div>
+                </div>
+            </div>
         </div>
+        
     );
 }
  
