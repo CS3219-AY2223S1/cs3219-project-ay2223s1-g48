db38a48f900e86a326b13cd041fcf3a8582e8d4d
Tan Yu Tao <tanyutao913@gmail.com>
2022-10-15 16:35:37 +0800

    modify get with new route

--- question-service/controller/question-controller.js
+++ question-service/controller/question-controller.js
@@ -1,91 +1,86 @@
 import {
-    ormCreateQuestion as _createQuestion,
-    ormGetQuestion as _getQuestion,
-    ormDeleteQuestion as _deleteQuestion,
-} from "../model/question-orm.js";
+  ormCreateQuestion as _createQuestion,
+  ormGetQuestion as _getQuestion,
+  ormDeleteQuestion as _deleteQuestion,
+} from '../model/question-orm.js';
+
+export async function getAllQuestion(req, res) {}
 
 export async function getQuestion(req, res) {
-    try {
-        const { difficulty } = req.body;
-        if (difficulty) {
-            const resp = await _getQuestion(difficulty);
-            console.log(resp);
-            if(resp.err) {
-                return res
-                    .status(400)
-                    .json({ message: "Could not retrieve Question"});
-            } else {
-                console.log(`retrieved question ${difficulty} successfully!`);
-                return res
-                    .status(200)
-                    .json({
-                        data: resp.question
-                    });
-            }
-        } else {
-            return res
-                .status(400)
-                .json({ message: "Difficulty is missing" })
-        }
-    } catch (err) {
-        return res
-            .status(500)
-            .json({ message: "Database failure when retrieving queston" });
+  try {
+    console.log(req.params.difficulty);
+    const difficulty = req.params.difficulty;
+    if (difficulty) {
+      const resp = await _getQuestion(difficulty);
+      console.log(resp);
+      if (resp.err) {
+        return res.status(400).json({ message: 'Could not retrieve Question' });
+      } else {
+        console.log(`retrieved question ${difficulty} successfully!`);
+        return res.status(200).json({
+          data: resp.question,
+        });
+      }
+    } else {
+      return res.status(400).json({ message: 'Difficulty is missing' });
     }
+  } catch (err) {
+    return res
+      .status(500)
+      .json({ message: 'Database failure when retrieving queston' });
+  }
 }
 
 export async function createQuestion(req, res) {
-    try {
-        const { difficulty, question } = req.body;
-        if (difficulty && question) {
-            const resp = await _createQuestion(difficulty, question);
-            console.log(resp);
-            if(resp.err) {
-                return res
-                    .status(400)
-                    .json({ message: "Could not create question"});
-            } else {
-                console.log(`Created new question difficulty: ${difficulty} successfully!`);
-                return res
-                    .status(201)
-                    .json({ message: `Created new question difficulty: ${difficulty} successfully!`});
-            }
-        } else {
-            return res
-                .status(400)
-                .json({ message: "Difficulty and/or Question are missing!"});
-        }
-    } catch (err) {
-        return res
-            .status(500)
-            .json({message: "Database failure when creating new user!" });
+  try {
+    const { difficulty, question } = req.body;
+    if (difficulty && question) {
+      const resp = await _createQuestion(difficulty, question);
+      console.log(resp);
+      if (resp.err) {
+        return res.status(400).json({ message: 'Could not create question' });
+      } else {
+        console.log(
+          `Created new question difficulty: ${difficulty} successfully!`
+        );
+        return res.status(201).json({
+          message: `Created new question difficulty: ${difficulty} successfully!`,
+        });
+      }
+    } else {
+      return res
+        .status(400)
+        .json({ message: 'Difficulty and/or Question are missing!' });
     }
+  } catch (err) {
+    return res
+      .status(500)
+      .json({ message: 'Database failure when creating new user!' });
+  }
 }
 
 export async function deleteQuestion(req, res) {
-    try {
-        const { index, difficulty } = req.body;
-        if (index && difficulty) {
-            const resp = await _deleteQuestion(index, difficulty);
-            console.log(resp);
-            if(resp.err) {
-                return res
-                    .status(400)
-                    .json({ message: "Could not delete question"});
-            } else {
-                console.log("Question deleted successfully!");
-                return res
-                    .status(200)
-                    .json({ message: "Question deleted successfully!"});
-            }
-        } else {
-            return res
-                .status(400)
-                .json({ message: "Index and/or difficulty are missing!"})
-        }
-    } catch (err) {
+  try {
+    const { index, difficulty } = req.body;
+    if (index && difficulty) {
+      const resp = await _deleteQuestion(index, difficulty);
+      console.log(resp);
+      if (resp.err) {
+        return res.status(400).json({ message: 'Could not delete question' });
+      } else {
+        console.log('Question deleted successfully!');
         return res
-            .status(500)
-            .json({ message: "Database failure when deleting question!"});
+          .status(200)
+          .json({ message: 'Question deleted successfully!' });
+      }
+    } else {
+      return res
+        .status(400)
+        .json({ message: 'Index and/or difficulty are missing!' });
     }
+  } catch (err) {
+    return res
+      .status(500)
+      .json({ message: 'Database failure when deleting question!' });
+  }
 }
--- question-service/index.js
+++ question-service/index.js
@@ -1,25 +1,27 @@
-import express from "express";
-import cors from "cors";
+import express from 'express';
+import cors from 'cors';
 
 const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors()); // config cors so that front-end can use
 import {
-    getQuestion,
-    createQuestion,
-    deleteQuestion,
-  } from "./controller/question-controller.js";
+  getQuestion,
+  createQuestion,
+  deleteQuestion,
+  getAllQuestion,
+} from './controller/question-controller.js';
 
 const router = express.Router();
 
-router.get("/", getQuestion);
-router.post("/", createQuestion);
-router.delete("/", deleteQuestion);
+router.get('/', getAllQuestion);
+router.get('/:difficulty', getQuestion);
+router.post('/', createQuestion);
+router.delete('/', deleteQuestion);
 
-app.use("/api/question", router).all((_, res) => {
-    res.setHeader("content-type", "application/json");
-    res.setHeader("Access-Control-Allow-Origin", "*");
-  });
-  
-  app.listen(8080, () => console.log("question-service listening on port 8080"));
+app.use('/api/question', router).all((_, res) => {
+  res.setHeader('content-type', 'application/json');
+  res.setHeader('Access-Control-Allow-Origin', '*');
+});
+
+app.listen(8080, () => console.log('question-service listening on port 8080'));
