23480f46c8c89e745ec8308abcc45b2840dc6e5f
Tan Yu Tao <tanyutao913@gmail.com>
2022-10-29 23:04:21 +0800

    change ref in config

--- frontend/src/configs.js
+++ frontend/src/configs.js
@@ -1,9 +1,10 @@
-const URI_USER_SVC = process.env.URI_USER_SVC || 'http://localhost:8000';
+const URI_USER_SVC =
+  process.env.REACT_APP_URI_USER_SVC || 'http://localhost:8000';
 
 const PREFIX_USER_SVC = '/api/user';
 
 const URI_QUESTION_SVC =
-  process.env.URI_QUESTION_SVC || 'http://localhost:8080';
+  process.env.REACT_APP_URI_QUESTION_SVC || 'http://localhost:8080';
 
 const PREFIX_QUESTION_SVC = '/api/question';
 
